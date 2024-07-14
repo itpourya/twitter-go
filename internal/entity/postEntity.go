2b26008060102dc292924d2b6f51781ea949f56d
Pourya Samimi <neverdiegenius@gmail.com>
2024-07-14 23:50:22 +0330

    Add: create new entity for posts and fix users

--- internal/entity/postEntity.go
+++ internal/entity/postEntity.go
@@ -1,1 +1,19 @@
 package entity
+
+type Post struct {
+	ID          int64     `gorm:"primary_key:auto_increment" json:"-"`
+	Content     string    `gorm:"type:varchar(128)" json:"-"`
+	AuthorEmail string    `gorm:"type:varchar(100)" json:"-"`
+	User        *User     `gorm:"foreignKey:ID"`
+	Bookmarks   []User    `gorm:"many2many:user_bookmarks;"`
+	Likes       []User    `gorm:"many2many:user_likes;"`
+	Comments    []Comment `gorm:"foreignKey:PostId;reference:ID;onDelete:CASCADE"`
+}
+
+type Comment struct {
+	ID      int64  `gorm:"primary_key:auto_increment" json:"-"`
+	Content string `gorm:"type:varchar(128)" json:"-"`
+	User    *User  `gorm:"foreignKey:ID"`
+	PostId  int    `gorm:"index"`
+	Post    *Post  `gorm:"foreignKey:ID"`
+}
--- internal/entity/userEntity.go
+++ internal/entity/userEntity.go
@@ -1,10 +1,19 @@
 package entity
 
 type User struct {
-	ID        int64  `gorm:"primary_key:auto_increment" json:"-"`
-	Firstname string `gorm:"type:varchar(100)" json:"-"`
-	Lastname  string `gorm:"type:varchar(100)" json:"-"`
-	Email     string `gorm:"type:varchar(100);unique" json:"-"`
-	Password  string `gorm:"type:varchar(100)" json:"-"`
-	IsActive  bool
+	ID         int64  `gorm:"primary_key:auto_increment" json:"-"`
+	Firstname  string `gorm:"type:varchar(100)" json:"-"`
+	Lastname   string `gorm:"type:varchar(100)" json:"-"`
+	Username   string `gorm:"type:varchar(100);unique" json:"-"`
+	Email      string `gorm:"type:varchar(100);unique" json:"-"`
+	Password   string `gorm:"type:varchar(100)" json:"-"`
+	IsActive   bool
+	Posts      []Post     `gorm:"foreignKey:ID;onDelete:CASCADE"`
+	Followers  []Follower `gorm:"foreignKey:ID"`
+	Followings []Follower `gorm:"foreignKey:ID"`
+}
+
+type Follower struct {
+	ID   int64 `gorm:"primary_key:auto_increment" json:"-"`
+	User *User `gorm:"foreignKey:ID"`
 }
