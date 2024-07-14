4e3aa4dca90e46c057a4acc9be709600bf0b1755
Pourya Samimi <neverdiegenius@gmail.com>
2024-07-14 03:40:01 +0330

    Add: create repository layer for API

--- internal/repository/authRepository.go
+++ internal/repository/authRepository.go
@@ -1,1 +1,52 @@
 package repository
+
+import (
+	"golang.org/x/crypto/bcrypt"
+	"gorm.io/gorm"
+	"log"
+	"twitter-go-api/internal/entity"
+)
+
+type AuthRepository interface {
+	AddUser(user entity.User) (string, error)
+	FindUserByEmail(email string) (entity.User, error)
+}
+
+type authRepository struct {
+	session *gorm.DB
+}
+
+func NewAuthRepository(session *gorm.DB) AuthRepository {
+	return &authRepository{
+		session: session,
+	}
+}
+
+func (a *authRepository) FindUserByEmail(email string) (entity.User, error) {
+	var user entity.User
+
+	query := a.session.Where("email == ?", email).Take(&user)
+	if query.Error != nil {
+		log.Println(query.Error)
+	}
+
+	return user, nil
+}
+
+func (a *authRepository) AddUser(user entity.User) (string, error) {
+	if user.Password != "" {
+		user.Password = hashAndSalt([]byte(user.Password))
+	}
+	a.session.Save(&user)
+
+	return "add user", nil
+}
+
+func hashAndSalt(pwd []byte) string {
+
+	hash, err := bcrypt.GenerateFromPassword(pwd, bcrypt.MinCost)
+	if err != nil {
+		log.Println(err)
+	}
+	return string(hash)
+}
