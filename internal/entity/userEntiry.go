78a1f1605b110b5430b17d4f9cff53b4ec1b472f
Pourya Samimi <neverdiegenius@gmail.com>
2024-07-14 03:36:06 +0330

    Build: create user models for database

--- internal/entity/userEntiry.go
+++ internal/entity/userEntiry.go
@@ -1,1 +1,10 @@
 package entity
+
+type User struct {
+	ID        int64  `gorm:"primary_key:auto_increment" json:"-"`
+	Firstname string `gorm:"type:varchar(100)" json:"-"`
+	Lastname  string `gorm:"type:varchar(100)" json:"-"`
+	Email     string `gorm:"type:varchar(100);unique" json:"-"`
+	Password  string `gorm:"type:varchar(100)" json:"-"`
+	IsActive  bool
+}
