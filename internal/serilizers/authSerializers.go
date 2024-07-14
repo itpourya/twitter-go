13b5e1efca069d1bb6c6c02985e261625c5b6e7e
Pourya Samimi <neverdiegenius@gmail.com>
2024-07-14 23:54:08 +0330

    Fix: change auth serializer and get username from users

--- internal/serilizers/authSerializers.go
+++ internal/serilizers/authSerializers.go
@@ -5,6 +5,7 @@ type RegisterRequest struct {
 	Lastname  string `json:"lastname" form:"lastname" binding:"required,min=1"`
 	Email     string `json:"email" form:"email" binding:"required,email"`
 	Password  string `json:"password" form:"password" binding:"required,min=4"`
+	Username  string `json:"username" form:"username" binding:"required,min=1"`
 }
 
 type LoginRequest struct {
