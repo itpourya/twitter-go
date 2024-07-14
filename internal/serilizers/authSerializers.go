0358840601348a9c1c8157c097fbd4c4df7e31c4
Pourya Samimi <neverdiegenius@gmail.com>
2024-07-14 03:41:07 +0330

    Add: create serializer for bind request data

--- internal/serilizers/authSerializers.go
+++ internal/serilizers/authSerializers.go
@@ -1,8 +1,18 @@
-package serializer
+package serilizers
 
 type RegisterRequest struct {
 	Firstname string `json:"firstname" form:"firstname" binding:"required,min=1"`
 	Lastname  string `json:"lastname" form:"lastname" binding:"required,min=1"`
 	Email     string `json:"email" form:"email" binding:"required,email"`
 	Password  string `json:"password" form:"password" binding:"required,min=4"`
+}
+
+type LoginRequest struct {
+	Email    string `json:"email" form:"email" binding:"required,email"`
+	Password string `json:"password" form:"password" binding:"required,min=4"`
+}
+
+type Token struct {
+	AccessToken  string `json:"access_token"`
+	RefreshToken string `json:"refresh_token"`
 }
