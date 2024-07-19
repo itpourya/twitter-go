acc7a7ab40e3c725884dad0cb4ab9791c8c9e5fe
Pourya Samimi <neverdiegenius@gmail.com>
2024-07-20 01:54:18 +0330

    Refactor: change post model

--- internal/entity/postEntity.go
+++ internal/entity/postEntity.go
@@ -4,16 +4,17 @@ type Post struct {
 	ID             int64     `gorm:"primary_key:auto_increment" json:"id"`
 	Content        string    `gorm:"type:varchar(128)" json:"content"`
 	AuthorUsername string    `gorm:"type:varchar(100)" json:"author_username"`
-	User           *User     `gorm:"foreignKey:ID" json:"-"`
+	User           User      `json:"-"`
+	UserID         uint      `gorm:"index" json:"-"`
 	Bookmarks      []User    `gorm:"many2many:user_bookmarks;" json:"-"`
 	Likes          []User    `gorm:"many2many:user_likes;" json:"-"`
-	Comments       []Comment `gorm:"foreignKey:PostId;reference:ID;onDelete:CASCADE" json:"-"`
+	Comments       []Comment `gorm:"many2many:user_comments;;onDelete:CASCADE" json:"-"`
 }
 
 type Comment struct {
-	ID      int64  `gorm:"primary_key:auto_increment" json:"-"`
-	Content string `gorm:"type:varchar(128)" json:"-"`
-	User    *User  `gorm:"foreignKey:ID"`
-	PostId  int    `gorm:"index"`
-	Post    *Post  `gorm:"foreignKey:ID"`
+	ID       int64  `gorm:"primary_key:auto_increment" json:"-"`
+	Content  string `gorm:"type:varchar(128)" json:"-"`
+	Username string
+	PostId   int   `gorm:"index"`
+	Post     *Post `gorm:"foreignKey:ID"`
 }
