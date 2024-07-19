b6b40ce694f8f659642f68130d61fd6f7f462454
Pourya Samimi <neverdiegenius@gmail.com>
2024-07-20 01:55:02 +0330

    Refactor: change user model and add follower and following model

--- internal/entity/userEntity.go
+++ internal/entity/userEntity.go
@@ -1,19 +1,32 @@
 package entity
 
+import "gorm.io/gorm"
+
 type User struct {
-	ID         int64  `gorm:"primary_key:auto_increment" json:"-"`
-	Firstname  string `gorm:"type:varchar(100)" json:"firstname"`
-	Lastname   string `gorm:"type:varchar(100)" json:"lastname"`
-	Username   string `gorm:"type:varchar(100);unique" json:"username"`
-	Email      string `gorm:"type:varchar(100);unique" json:"email"`
-	Password   string `gorm:"type:varchar(100)" json:"-"`
-	IsActive   bool
-	Posts      []Post     `gorm:"foreignKey:ID;onDelete:CASCADE"`
-	Followers  []Follower `gorm:"foreignKey:ID"`
-	Followings []Follower `gorm:"foreignKey:ID"`
+	gorm.Model
+	ID        int    `gorm:"primary_key:auto_increment" json:"-"`
+	Firstname string `gorm:"type:varchar(100)" json:"firstname"`
+	Lastname  string `gorm:"type:varchar(100)" json:"lastname"`
+	Username  string `gorm:"type:varchar(100);unique" json:"username"`
+	Email     string `gorm:"type:varchar(100);unique" json:"email"`
+	Password  string `gorm:"type:varchar(100)" json:"-"`
+	IsActive  bool
 }
 
 type Follower struct {
-	ID   int64 `gorm:"primary_key:auto_increment" json:"-"`
-	User *User `gorm:"foreignKey:ID"`
+	gorm.Model
+	ID             int  `gorm:"primary_key:auto_increment" json:"id"`
+	UserID         int  `gorm:"not null"`
+	FollowerUserID int  `gorm:"not null"`
+	User           User `gorm:"foreignKey:UserID"`
+	Follower       User `gorm:"foreignKey:FollowerUserID"`
+}
+
+type Following struct {
+	gorm.Model
+	ID              int  `gorm:"primary_key:auto_increment" json:"id"`
+	UserID          int  `gorm:"not null"`
+	FollowingUserID int  `gorm:"not null"`
+	User            User `gorm:"foreignKey:UserID"`
+	Following       User `gorm:"foreignKey:FollowingUserID"`
 }
